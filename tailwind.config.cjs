a2330833604c460e8f020fa3654691b68387c0a5
Daniel Belo Goncalves <danigo99@gmail.com>
2023-08-19 17:40:08 +0100

    simplify the whole thing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", 'Inter', 'sans-serif']
+      },
+      spacing: {
+        '128': '32rem'
+      },
+      borderRadius: {
+        'container': '2rem'
+      },
+      colors: {
+        background: "#f7f2f2",
+        darkGrey: "#1a1a1a"
+      }
+    }
+  },
+  plugins: [],
 }
